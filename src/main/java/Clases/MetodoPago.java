505f24fa2ddba6e2059a45d80cd2e5b7a5545353
DIOC2888 <denisivanorcas@gmail.com>
2026-04-14 14:39:22 -0600

    Creacion de paquetes y clases

--- src/main/java/Clases/MetodoPago.java
+++ src/main/java/Clases/MetodoPago.java
@@ -2,6 +2,13 @@ package Clases;
 
 public class MetodoPago {
 
+    double tarjeta;
+    double efectivo;
+
+        public MetodoPago(double tarjeta,double efectivo){
+            this.tarjeta = tarjeta;
+            this.efectivo = tarjeta;
+        }
 
 
 }
